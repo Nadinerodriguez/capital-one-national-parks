3ba546818307500d228f98ba6c7ccc4561353980
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2019-05-27 00:53:02 -0400

    Add Homepage

--- nationalpark/urls.py
+++ nationalpark/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import park.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', park.views.index, name='index'),
 ]
