e33ea31cce3eb7f8ed80a231107e6090b550245a
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2019-06-02 04:34:14 -0400

    Changed gitignore

---
+++ park/api_keys.py
@@ -0,0 +1,7 @@
+""" This file holds all api keys. Not pushed onto github """
+
+def get_nps_api_key():
+    return "BfjJkQ8FsI9gI33vN95G8f2tunWF5qZFYBZFOwb0"
+
+def get_mapbox_access_token():
+    return "pk.eyJ1IjoibmFkaW5lcm9kcmlndWV6IiwiYSI6ImNqd2NuZHlqajB4Y2o0OGxqdnQ3bnprcmEifQ.ey9ZbNs3yebADmJ1EGPJUg"
