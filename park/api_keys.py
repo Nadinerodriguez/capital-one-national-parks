69225b5e0d664ad14cf42310eaefea3314b1aa7b
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2019-06-02 20:20:46 -0400

    make api keys private?

--- park/api_keys.py
+++
@@ -1,7 +0,0 @@
-""" This file holds all api keys. """
-
-def get_nps_api_key():
-    return "BfjJkQ8FsI9gI33vN95G8f2tunWF5qZFYBZFOwb0"
-
-def get_mapbox_access_token():
-    return "pk.eyJ1IjoibmFkaW5lcm9kcmlndWV6IiwiYSI6ImNqd2NuZHlqajB4Y2o0OGxqdnQ3bnprcmEifQ.ey9ZbNs3yebADmJ1EGPJUg"
